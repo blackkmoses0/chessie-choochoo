897b3f60370a879575ccc186e9d7b7ab6ad2995d
Andrew Evans <45642358+andrewevans0102@users.noreply.github.com>
2019-09-30 14:33:20 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  stationInformation: 'https://ms-metro.azurewebsites.net/station-information',
+  arrivalTimes: 'https://ms-metro.azurewebsites.net/arrival-times',
+  stationHours: 'https://ms-metro.azurewebsites.net/station-hours',
+  stationRoute: 'https://ms-metro.azurewebsites.net/station-route',
+  stationList: 'https://ms-metro.azurewebsites.net/station-list'
 };
